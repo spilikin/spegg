a86a1f5bd291c44b1c74989bbeaab65f72e7e201
spilikin <108782+spilikin@users.noreply.github.com>
2021-05-13 15:53:07 +0200

    Update __init__.py

--- spegg/api/__init__.py
+++ spegg/api/__init__.py
@@ -1,6 +1,7 @@
-__version__='0.14.0'
+__version__='0.14.1'
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .api import api
 
 app = FastAPI(
@@ -10,7 +11,23 @@ app = FastAPI(
     docs_url=None,
     redoc_url='/api/docs'
     )
+
+origins = [
+    "https://spilikin.grafana.net",
+    "https://spegg.spilikin.dev/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api, prefix="/api/v1")
+
+
 
 def serve():
     uvicorn.run("spegg.api:app", reload=True, workers=2)
